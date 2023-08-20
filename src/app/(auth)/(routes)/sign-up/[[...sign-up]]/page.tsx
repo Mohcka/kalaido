ddd8d5670735eb9d3f03039d575ff553f95f11f1
Mohcka <MohckaMas@proton.me>
2023-08-20 15:50:46 -0400

    updates image 🖼️

--- src/app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -251,7 +251,7 @@ export default function SignUp() {
         <div className="relative hidden w-0 flex-1 lg:block">
           <div className="absolute inset-0 h-full w-full">
             <Image
-              src="https://images.unsplash.com/photo-1620712943543-bcc4688e7485?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=930&q=80"
+              src="/robo.webp"
               alt=""
               className="object-cover"
               fill
